df58aa0522633bbf70b6faaff570da0f301e7f9a
Kaining <kainingchen2013@gmail.com>
2023-03-01 16:03:25 -0500

    Feature 11 complete

--- todos/views.py
+++ todos/views.py
@@ -45,3 +45,11 @@ def todo_list_update(request, id):
         "form": form,
     }
     return render(request, "todos/update.html", context)
+
+def todo_list_delete(request, id):
+    todo_list = get_object_or_404(TodoList, id=id)
+    if request.method == "POST":
+        todo_list.delete()
+        return redirect("todo_list_list")
+
+    return render(request, "todos/delete.html")
