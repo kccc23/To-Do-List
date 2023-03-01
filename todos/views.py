17dffab8b26962f6f738e298e71e786851637afb
Kaining <kainingchen2013@gmail.com>
2023-03-01 13:30:12 -0500

    Feature 8 complete

--- todos/views.py
+++ todos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from todos.models import TodoList, TodoItem
 
 # Create your views here.
@@ -9,3 +9,10 @@ def todo_list_list(request):
         "todo_lists": todo_lists,
     }
     return render(request, "todos/list.html", context)
+
+def todo_list_detail(request, id=id):
+    todo_list = get_object_or_404(TodoList, id=id)
+    context = {
+        "todo_list": todo_list,
+    }
+    return render(request, "todos/detail.html", context)
